5cebfb1861f94334957291ded835226a0dce699c
teikenbun <ramzelsuarez@yahoo.com>
2026-07-21 16:34:23 +0900

    InputSystems_WIP
    Work the rest at home. Update by then.

---
+++ Source/ProjectAxiom/Private/Player/AxiomPlayerController.cpp
@@ -0,0 +1,85 @@
+﻿// Copyright Zel Suarez
+
+#include "Player/AxiomPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAxiomPlayerController::AAxiomPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAxiomPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		Subsystem->AddMappingContext(AxiomIMC, 0);
+	}
+	
+}
+
+void AAxiomPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* AxiomInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	AxiomInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+	AxiomInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Input_Look);
+	AxiomInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Input_Jump);
+	AxiomInputComponent->BindAction(CrouchAction, ETriggerEvent::Started, this, &ThisClass::Input_Crouch);
+}
+
+void AAxiomPlayerController::Input_Crouch()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	if (UCharacterMovementComponent* CMC = GetCharacter()->GetCharacterMovement(); IsValid(CMC))
+	{
+		CMC->bWantsToCrouch = !CMC->bWantsToCrouch;
+	}
+}
+
+void AAxiomPlayerController::Input_Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+	UCharacterMovementComponent* CMC = GetCharacter()->GetCharacterMovement(); 
+	if (!IsValid(CMC)) return;
+	
+	if (CMC->bWantsToCrouch)
+	{
+		CMC->bWantsToCrouch = false;
+	}
+	else
+	{
+		GetCharacter()->Jump();
+	}
+	
+}
+
+void AAxiomPlayerController::Input_Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(Rotation).GetUnitAxis(EAxis::Y);
+	
+	if (APawn* ControlledPawn = GetPawn())
+	{
+		
+	}
+}
+
+void AAxiomPlayerController::Input_Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	AddYawInput(InputAxisVector.X);
+	AddPitchInput(InputAxisVector.Y);
+}
---
+++ Source/ProjectAxiom/Public/Player/AxiomPlayerController.h
@@ -0,0 +1,45 @@
+﻿// Copyright Zel Suarez
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AxiomPlayerController.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
+UCLASS()
+class PROJECTAXIOM_API AAxiomPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AAxiomPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+private:
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputMappingContext> AxiomIMC;
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputAction> CrouchAction;
+	
+	UPROPERTY(EditAnywhere, Category = "FPS|Input")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	void Input_Crouch();
+	void Input_Jump();
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+};
